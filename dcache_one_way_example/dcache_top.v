6d6000964cc0d3c7ebf5b7db9f5c9595e912a0f6
eugenechen-ourtimes <eugene.chen19971108@gmail.com>
2019-01-06 17:31:22 +0800

    dcache_top

--- dcache_one_way_example/dcache_top.v
+++ dcache_one_way_example/dcache_top.v
@@ -173,17 +173,21 @@ always@(posedge clk_i or negedge rst_i) begin
 			end
 			STATE_MISS: begin
 				if(sram_dirty) begin		//write back if dirty
-	                //!!! add you code here!
+					mem_enable <= 1;
+					mem_write <= 1;
+					write_back <= 1;
 					state <= STATE_WRITEBACK;
 				end
 				else begin					//write allocate: write miss = read miss + write hit; read miss = read miss + read hit
-	                //!!! add you code here!
+					mem_enable <= 1;
+					mem_write <= 0;
 					state <= STATE_READMISS;
 				end
 			end
 			STATE_READMISS: begin
 				if(mem_ack_i) begin			//wait for data memory acknowledge
-	                //!!! add you code here!
+					cache_we <= 1;
+					mem_enable <= 0;
 					state <= STATE_READMISSOK;
 				end
 				else begin
@@ -191,12 +195,14 @@ always@(posedge clk_i or negedge rst_i) begin
 				end
 			end
 			STATE_READMISSOK: begin			//wait for data memory acknowledge
-	                //!!! add you code here!
-				state <= STATE_IDLE;
+					cache_we <= 0;
+					state <= STATE_IDLE;
 			end
 			STATE_WRITEBACK: begin
 				if(mem_ack_i) begin			//wait for data memory acknowledge
-	                //!!! add you code here!
+					mem_enable <= 1;
+					write_back <= 0;
+					mem_write <= 0;
 					state <= STATE_READMISS;
 				end
 				else begin
